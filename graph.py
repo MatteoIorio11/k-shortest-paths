9330ad5dd6e346cc555727f5c796fa0105d45791
Matteo <matteo.iorio01@gmail.com>
2023-04-22 17:48:33 +0200

    fixed notebook

--- graph.py
+++ graph.py
@@ -1,20 +1,20 @@
 class Vertex:
     def __init__(self, vertex: int):
         self.vertex = vertex
-        self.neighboors = {}
+        self.neighbors = {}
 
-    def getNeighboors(self):
-        return self.neighboors
+    def getneighbors(self):
+        return self.neighbors
     
     def getVertex(self):
         return self.vertex
     
     def getCost(self, other: int):
-        return self.neighboors.get(other, float('inf'))
+        return self.neighbors.get(other, float('inf'))
     
-    def addNeighboor(self, neigh, cost: int):
-        if neigh not in self.neighboors:
-            self.neighboors.setdefault(neigh, cost)
+    def addneighbor(self, neigh, cost: int):
+        if neigh not in self.neighbors:
+            self.neighbors.setdefault(neigh, cost)
     
     def __eq__(self, other):
         return isinstance(other, Vertex) and self.vertex == other.getVertex()
@@ -31,6 +31,7 @@ class Graph:
     vertexs = {}
     def __init__(self):
         self.size = 0
+        self.vertexs = {}
     
     def addVertex(self, vertex: int):
         if vertex not in self.vertexs:
@@ -40,8 +41,9 @@ class Graph:
     
     def addArch(self, v1: int, v2: int, cost: int):
         if cost > 0:
-            if v1 in self.vertexs:
-                self.vertexs[v1].addNeighboor(Vertex(v2), cost)
+            if v1 not in self.vertexs:
+                self.addVertex(v1)
+            self.vertexs[v1].addneighbor(Vertex(v2), cost)
                 
     def getVertex(self, v: int):
         if v in self.vertexs:
@@ -52,10 +54,24 @@ class Graph:
     def getSize(self):
         return self.size
 
+
+
+
 import heapq
 import numpy as np
 
+
 def Dijkstra(graph: Graph, start: int, destination: int):
+    """_summary_
+
+    Args:
+        graph (Graph): Graph, where all the vertex are stored
+        start (int): starting vertex
+        destination (int): destination vertex
+
+    Returns:
+        list : path from start to destination
+    """
     if graph.getVertex(start) == None or graph.getVertex(destination) == None:
         return []
     l_i = np.full(graph.getSize()+1, float('inf'))
@@ -68,48 +84,50 @@ def Dijkstra(graph: Graph, start: int, destination: int):
     while curr_vertex != destination and len(heap) > 0:
         cost, curr_vertex = heapq.heappop(heap)
         if curr_vertex != destination:
-            for vertex_value, cost in graph.getVertex(curr_vertex).getNeighboors().items():
+            for vertex_value, cost in graph.getVertex(curr_vertex).getneighbors().items():
                 distance = l_i[curr_vertex] + cost
-                print(f"Neigh {vertex_value} Current {curr_vertex} Cost {cost}")
                 if l_i[vertex_value.getVertex()] > distance:
                     l_i[vertex_value.getVertex()] = distance
                     heapq.heappush(heap, (l_i[vertex_value.getVertex()], vertex_value.getVertex()))
                     Pred[vertex_value.getVertex()] = curr_vertex
-            
-    print(Pred)
-    
+        
     def build_path():
         path = [destination]
-        print(destination)
         curr_vertex = destination
         while curr_vertex != start:
             curr_vertex = Pred[curr_vertex]
-            print(curr_vertex)
             path.append(curr_vertex)
         
         path.reverse()
         return path
         
-        
     return build_path()
 
 
-import csv
+def K_Dijkstra(graph: Graph, start: int, destination: int, k: int):
+    """_summary_
 
-graph = Graph()
+    Args:
+        graph (Graph): Graph, where all the vertex are stored
+        start (int): starting Vertex
+        destination (int): destination Vertex
+        k (int): how many paths must be generated
 
+    Returns:
+        list(list): where all the k-paths are stored from start to destination 
+    """
+    paths = []
+    heap = [(0, [], start)]
+    while heap and len(paths) < k:
+        cost, path, current_node = heapq.heappop(heap)
+        path = path + [current_node]
+        if current_node and current_node == destination:
+            paths.append(path)
+        elif graph.getVertex(current_node):
+            for neighbor, weight in graph.getVertex(current_node).getneighbors().items():
+                if neighbor.getVertex() not in path:
+                    heapq.heappush(heap, (cost + weight, path, neighbor.getVertex()))
 
-with open('graph.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        v1 = int(row[0])
-        v2 = int(row[1])
-        cost = int(row[2])
-        graph.addVertex(v1)
-        graph.addArch(v1, v2, cost)
-        print(f"Node : Node1 {v1}, Graph Size: {graph.getSize()}")
+    return paths
 
-print(graph.getSize())
-
-print(Dijkstra(graph, 1, 4))
 
